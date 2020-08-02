2ef2f58a2828333e1f9dbec20dcc6334bc04d9de
ecoltri2 <edr.ccol@gmail.com>
2020-08-02 12:06:55 -0300

    Updating User

--- src/app/users/routes/user.router.ts
+++ src/app/users/routes/user.router.ts
@@ -5,30 +5,51 @@ class UsersRouter extends Router {
     applyRoutes(application: restify.Server) {
 
         application.get('/users', (req, resp, next) => {
-            UserDTO.find().then(data =>{
+            UserDTO.find().then(data => {
                 resp.json(data)
                 return next()
             })
         })
 
         application.get('/users/:id', (req, resp, next) => {
-            UserDTO.findById(req.params['id']).then(data =>{
-                if(data){
+            UserDTO.findById(req.params['id']).then(data => {
+                if (data) {
                     resp.json(data)
                     return next()
                 }
-                resp.send(404, {message:"NOT FOUND"})   
+                resp.send(404, { message: "NOT FOUND" })
                 return next()
             })
         })
 
-        application.post('/users',(req,resp,next)=>{
+        application.post('/users', (req, resp, next) => {
             let user = new UserDTO(JSON.parse(req.body))
             user.save().then(userCreated => {
                 userCreated.password = 'encrypted'
                 resp.json(userCreated)
                 return next()
             })
+        })
+
+        application.put('/users/:id', (req, resp, next) => {
+            const options = {
+                overwrite: true
+            }
+            UserDTO.update({ "_id": req.params['id'] }, JSON.parse(req.body))
+                .exec()
+                .then(result => {
+                    if(result.n){
+                        return UserDTO.findById(req.params['id'])
+                    }
+                    return resp.send(404)
+                })
+                .then(user => {
+                    resp.json(user)
+                    return next()
+                })
+                .catch(err => 
+                    resp.send(500,err)
+                )
         })
     }
 }
